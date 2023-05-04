82362752137518de40662014c629a37d8ae56b0f
mariusd <mariusd@examplel.com>
2023-05-04 07:12:18 +0300

    add backpack component

--- src/Backpack.tsx
+++ src/Backpack.tsx
@@ -1,38 +1,39 @@
-// import React from 'react';
-// import { useEventCloud } from './EventCloudContext';
+import React from 'react';
+import { useEventCloud } from './EventCloudContext';
 
+import './Backpack.css';
 
-// const Backpack = () => {
-//   const { backpack } = useEventCloud();
+const Backpack = () => {
+  const { backpack } = useEventCloud();
 
-//   const renderGridItems = () => {
-//     if (backpack === null) {
-//       return <p>No backpack data available</p>;
-//     }
+  const renderGridItems = () => {
+    if (backpack === null) {
+      return <p>No backpack data available</p>;
+    }
 
-//     const gridItems = [];
-//     for (let y = 0; y < backpack.grid.length; y++) {
-//       for (let x = 0; x < backpack.grid[y].length; x++) {
-//         const isOccupied = backpack.grid[y][x];
-//         gridItems.push(
-//           <div
-//             key={`${x},${y}`}
-//             className={`${styles.gridItem} ${isOccupied ? styles.occupied : ''}`}
-//           >
-//             <div className={styles.square}></div>
-//           </div>
-//         );
-//       }
-//     }
-//     return gridItems;
-//   };
+    const gridItems = [];
+    for (let y = 0; y < backpack.grid.length; y++) {
+      for (let x = 0; x < backpack.grid[y].length; x++) {
+        const isOccupied = backpack.grid[y][x];
+        gridItems.push(
+          <div
+            key={`${x},${y}`}
+            className={`gridItem ${isOccupied ? 'occupied' : ''}`}
+          >
+            <div className="square"></div>
+          </div>
+        );
+      }
+    }
+    return gridItems;
+  };
 
-//   return (
-//     <div>
-//       <h2>Backpack</h2>
-//       <div className={styles.grid}>{renderGridItems()}</div>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h2>Backpack</h2>
+      <div className="grid">{renderGridItems()}</div>
+    </div>
+  );
+};
 
-// export default Backpack;
+export default Backpack;
