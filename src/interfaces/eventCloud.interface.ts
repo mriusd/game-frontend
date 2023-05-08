db9d42bb7592cdc7905db025831fced050eda1e3
mariusd <mariusd@examplel.com>
2023-05-08 15:28:26 +0300

    split submit Attack into two functions

--- src/interfaces/eventCloud.interface.ts
+++ src/interfaces/eventCloud.interface.ts
@@ -2,6 +2,7 @@ import type { Fighter } from "./fighter.interface";
 import type { Coordinate } from "./coordinate.interface";
 import { BackpackSlot } from './backpack.interface';
 import type { ItemDroppedEvent } from "./item.interface"; 
+import type { Direction } from "./direction.interface"; 
 
 export interface EventCloud {
     PlayerID: any 
@@ -15,7 +16,8 @@ export interface EventCloud {
     money: any 
     equipment: Record<number, BackpackSlot> 
     moveFighter: (coordinate: Coordinate) => void 
-    submitAttack: any
+    submitSkill: (direction: Direction) => void
+    submitMalee: (direction: Direction) => void
     target: any
     setTarget: (targetId: number | string) => void
     refreshFighterItems: any
